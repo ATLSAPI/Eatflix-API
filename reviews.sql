7772f9c82249b164db1eabd02492de9994f53025
Melvin <mconi2007@yahoo.com>
2015-02-09 11:01:31 +0000

    First Commit

---
+++ reviews.sql
@@ -0,0 +1,3 @@
+INSERT INTO reviews (id, description, user_id, restaurant_id, rating, created, modified) VALUES (1, 'Great food, poor service   ', 1, 1, 3, '15/01/2015 ', '15/01/2015 ');
+INSERT INTO reviews (id, description, user_id, restaurant_id, rating, created, modified) VALUES (2, 'Bad food', 1, 1, 4, '13/01/2015 ', '15/01/2015 ');
+INSERT INTO reviews (id, description, user_id, restaurant_id, rating, created, modified) VALUES (3, 'New review', 1, 1, 5, '14/01/2015 ', '15/01/2015 ');
